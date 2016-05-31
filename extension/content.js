641d899245df38bc24727a214312fc69ad444ef2
BenjiFischman <fisch885@umn.edu>
2016-05-31 18:56:16 -0500

    Update content.js

--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,6 @@
+
 chrome.runtime.onMessage.addListener(
+
     function(request, sender, sendResponse) {
         if (request.message === 'get_titles') {
             // get titles from page
@@ -8,8 +10,24 @@ chrome.runtime.onMessage.addListener(
                 var aTag = headerTags[i].getElementsByTagName('a');
                 titles.push(aTag[0].innerText);
             }
+
             // send titles back to background for posting
             chrome.runtime.sendMessage({'message': 'post_titles', 'titles': titles});
+
+            if (document.readyState === 'complete') {
+            //Navigates to next page
+                var urlTags = document.getElementById('gs_n');
+                urlTags = urlTags.getElementsByTagName('a');
+                var urls = [];
+                for (var i = 0; i < urlTags.length; i++) {
+                     var aUrl = urlTags[i].getAttribute('href');
+                      urls.push(aUrl);
+                  }
+                 var newUrl = urls.slice(-1)[0];
+                 window,location.assign(newUrl);
+
+            }
+
         }
     }
 )
